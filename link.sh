f17daddce3c395e53361d38fb9750691f87e8435
Hirano Tatsuya <k017c1066@it-neec.jp>
2019-07-24 16:29:00 +0900

    Add .tmux.conf

--- link.sh
+++ link.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd
-ln -fs dotfiles/.vimrc .vimrc
-ln -fs dotfiles/.vim .vim
+ln -fs SCRIPT_DIR/.vimrc .vimrc
+ln -fs SCRIPT_DIR/.vim .vim
+ln -fs SCRIPT_DIR/misc/.tmux.conf .tmux.conf
+ln -fs SCRIPT_DIR/misc/.gitconfig .gitconfig
+ln -fs SCRIPT_DIR/bash/.bashrc .bashrc
 if [ -n $XDG_CONFIG_HOME ]; then
-    ln -fs dotfiles/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+    ln -fs SCRIPT_DIR/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 else
-    ln -fs dotfiles/.vimrc ~/.config/nvim/init.vim
+    ln -fs SCRIPT_DIR/.vimrc ~/.config/nvim/init.vim
 fi
