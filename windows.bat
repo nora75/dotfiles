49442174548898c5b1d3254206b224059a1b1359
Hirano Tatsuya <k017c1066@it-neec.jp>
2020-04-01 17:06:05 +0900

    [Fix] Can't link ~/.bash_local when windows
    [windows.bat]
    * Can't link ~/.bash_local

--- windows.bat
+++ windows.bat
@@ -1,11 +1,16 @@
 @ehco off
+FOR /F %%i in ('hostname') do set myhost=%%i
+
 rd /s /q "%USERPROFILE%\.vimrc"
 rd /s /q "%USERPROFILE%\.vim"
 rd /s /q "%USERPROFILE%\.tmux.conf"
 rd /s /q "%USERPROFILE%\.bashrc"
 rd /s /q "%USERPROFILE%\.bash_local"
-mklink /d "%USERPROFILE%\.vim" "%USERPROFILE%\dotfiles\.vim"
-mklink "%USERPROFILE%\.vimrc" "%USERPROFILE%\dotfiles\.vimrc"
-mklink "%USERPROFILE%\.bashrc" "%USERPROFILE%\dotfiles\.bashrc"
-mklink "%USERPROFILE%\.tmux.conf" "%USERPROFILE%\dotfiles\.tmux.conf"
+
+mklink /d "%USERPROFILE%\.vim" "%USERPROFILE%\dotfiles\vim\.vim"
+mklink "%USERPROFILE%\.vimrc" "%USERPROFILE%\dotfiles\vim\.vimrc"
+mklink "%USERPROFILE%\.bashrc" "%USERPROFILE%\dotfiles\bash\.bashrc"
+mklink "%USERPROFILE%\.tmux.conf" "%USERPROFILE%\dotfiles\misc\.tmux.conf"
 mklink "%USERPROFILE%\AppData\Local\nvim\init.vim" "%USERPROFILE%\dotfiles\.vimrc"
+
+if exist  "%USERPROFILE%\dotfiles\bash\%myhost%" mklink "%USERPROFILE%\.bash_local" "%USERPROFILE%\dotfiles\bash\%myhost%"
